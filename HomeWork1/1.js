7127ec5e5b19c2a8fc2f1a4d052a8f7195b931ef
Aleksei <alex18w@mail.ru>
2023-08-26 19:20:58 +0500

    HomeWork1 Task1

--- HomeWork1/1.js
+++ HomeWork1/1.js
@@ -13,3 +13,6 @@
 Уточнение: пользователь всегда вводит корректное число.
 */
 
+Math.round
+let user_input = Number.parseInt(prompt('Введите температуру в градусах Цельсия'));
+alert(`Цельсия: ${user_input}\nФаренгейт: ${Math.round((9 / 5) * user_input + 32)}`)
